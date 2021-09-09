afa645e1a52642d7e0185c8102b6b7718be157d1
Dmytro Honcharov <dmytro.honcharov@accellion.con>
2021-09-09 06:20:51 -0700

    Homework-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from "./components/Layout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LayoutImage from './assets/bg1.jpg'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title='Title 0' descr='Here is our description' />
+      <Layout title='Title 1' desc='First Layout' urlBg={LayoutImage} />
+      <Layout title='Title 2' desc='Second Layout' colorBg='red' />
+      <Layout title='Title 3' desc='Third Layout' urlBg={LayoutImage} />
+      <Footer />
+    </>
   );
 }
 
