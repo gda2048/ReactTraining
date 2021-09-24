e18b7dc5d6e7295625cdb85b04d9e88bbaedde21
Dmytro Honcharov <dmytro.honcharov@accellion.con>
2021-09-24 14:56:53 -0700

    task-2

--- src/components/routes/Game/routes/Finish/index.js
+++ src/components/routes/Game/routes/Finish/index.js
@@ -1,15 +1,32 @@
 import '../../../button.css'
 import s from "./style.module.css";
 import PokemonCard from "../../../../PokemonCard";
+import {FirebaseContext} from "../../../../../context/firebaseContext";
 import {PokemonContext} from "../../../../../context/pokemonContext";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {useHistory} from "react-router-dom";
 
 const FinishPage = () => {
+    const firebase = useContext(FirebaseContext);
     const {pokemons} = useContext(PokemonContext);
     const history = useHistory();
+    const [cardID, setCardID] = useState(null)
+    const [card, setCard] = useState(null)
 
-    const finishGame = () => {
+    const handleClick = (item) => {
+        if (item.id === cardID){
+            setCardID(null);
+            setCard(null);
+        } else {
+            setCardID(item.id);
+            setCard(item);
+        }
+    }
+    console.log('card', card)
+    const finishGame = async () => {
+        if (cardID){
+            await firebase.addPokemon(card, () => {})
+        }
         pokemons.p1 = []
         pokemons.p2 = []
         pokemons.isFinished = false
@@ -19,7 +36,7 @@ const FinishPage = () => {
     if (!pokemons.isFinished){
         finishGame()
     }
-    console.log(pokemons)
+
     return (
         <div>
             <div className={s.flex}>
@@ -35,8 +52,11 @@ const FinishPage = () => {
             <div className={s.flex}>
                 {
                     pokemons.p2.map((item) =>
-                        <PokemonCard key={item.key} values={item.values} className={s.card}
-                                     name={item.name} type={item.type} id={item.id} img={item.img} isActive minimize/>
+                        <div onClick={() => handleClick(item)} className={s.card}>
+                            <PokemonCard key={item.key} values={item.values} name={item.name}
+                                         type={item.type} id={item.id} img={item.img} isSelected={item.id===cardID}
+                                         isActive minimize/>
+                        </div>
                     )
                 }
             </div>
